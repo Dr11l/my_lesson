ca6c6b3e215bf3051ccd80176c40fc1db39efd91
Aktam <iev_ak@mail.ru>
2024-03-11 17:58:42 +0300

    my first commit

--- src/Lesson8/Car.java
+++ src/Lesson8/Car.java
@@ -1,18 +1,17 @@
 package Lesson8;
 
 public class Car {
-    String color;
-    String engine;
+    String color = "blue";
+    String engine = "V6";
 }
 
 class Human {
     String name = "Ivan";
-    Car c = new Car();
+    final Car c = new Car();
 
     public static void main(String[] args) {
         Human h1 = new Human();
-        h1.c = new Car();
-        h1.c = new Car();
-        h1.c.engine = "V6";
+        h1.c.engine = "V8";
+        System.out.println(h1.c.engine);
     }
 }
---
+++ src/Lesson8/Student.java
@@ -0,0 +1,40 @@
+package Lesson8;
+
+public class Student {
+    String name;
+    int course;
+    static int count;
+    int a;
+
+    public Student(String name2, int course2) {
+        count++;
+        name = name2;
+        course = course2;
+        System.out.println("Student # " + count + " sozdan");
+    }
+
+    public static void showCount() {
+        System.out.println("Vsego sozdano studentov " + count);
+    }
+
+    public void showInfo() {
+        System.out.println("Welcome to the Student class");
+    }
+
+    void abc() {
+        a++;
+    }
+
+    static void abcd() {
+        count++;
+        Student st2 = new Student("Petr", 3);
+        st2.a++;
+    }
+
+
+    public static void main(String[] args) {
+    abcd();
+        Student st3= new Student("Maks", 6);
+        st3.abc();
+    }
+}
