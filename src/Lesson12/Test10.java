a18c8b734594852b70c808cbaf9911cbdc79a5c7
Aktam <iev_ak@mail.ru>
2024-03-19 20:50:43 +0300

    fwaa

---
+++ src/Lesson12/Test10.java
@@ -0,0 +1,88 @@
+package Lesson12;
+
+public class Test10 {
+
+
+    void abc() {
+
+
+        String str;
+        int a = 10;
+        if (a >= 10) {
+            str = "Privet";
+        }
+        if (a < 1) {
+            str = "Poka";
+        } else {
+            str = "wpqdlqwpdq";
+        }
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        int maximum = (a > b) ? a : b;
+        System.out.println(maximum);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
